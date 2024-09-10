3e0ddec8e564ec97d41cb223ce33bcd837f19bf1
Stas1589 <ingredients24@gmail.com>
2024-09-10 19:37:57 +0300

    New

--- tester_files.py
+++ tester_files.py
@@ -12,17 +12,5 @@
 # file.close()
 # print(file.closed)
 
-# with open(r'test.txt', 'r', encoding='utf8') as file:
-#     # data = file.read(10)
-#     # print(data)
-#     data = file.readline()
-#     print(data)
-#     data2 = file.readline()
-#     print(data2)
-#     while len(data) > 0:
-#         data = file.readline()
-#         print(data)
-
-# with open('test.txt'/test45, 'rb') as file:
-#     data
+print(123)
 
